94d7db415e9bccd2f335aa203e25b0eb8ded213e
Rachel Wong <rachelwong.ly@gmail.com>
2019-01-17 16:15:32 +0000

    added user registration page and users object

--- express_server.js
+++ express_server.js
@@ -14,11 +14,23 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+
+}
+
 generateRandomString = () => Math.random().toString(36).substring(2, 8)
-
-
-
-
 
 
 app.get("/", (req, res) => {
@@ -83,12 +95,9 @@ app.get(`/urls/:id/edit`, (req, res) => {
 });
 
 
-
 app.post("/urls/:id/edit", (req, res) => {
-  // console.log(newLongURL);
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls');
-  // console.log(urlDatabase);
 });
 
 app.post("/login", (req, res) => {
@@ -101,7 +110,13 @@ app.post("/logout", (req, res) => {
   res.redirect('urls');
 });
 
+app.get("/register", (req, res) => {
+  res.render("register-user");
+})
 
+app.post("/register", (req, res) => {
+  // res.redirect();
+})
 
 
 app.listen(PORT, () => {
