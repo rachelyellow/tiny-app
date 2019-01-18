f7b3234d4ccd352da621daf12b58ae9798af7178
Rachel Wong <rachelwong.ly@gmail.com>
2019-01-18 23:57:26 +0000

    completed cookie encryption

--- express_server.js
+++ express_server.js
@@ -1,13 +1,18 @@
 const bcrypt = require('bcrypt');
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const express = require("express");
 const PORT = 8080; // default port 8080
 const app = express();
 
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['abcdef'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
 app.set('view engine', 'ejs');
 
 
@@ -47,13 +52,13 @@ app.get("/urls", (req, res) => {
 
 
   let templateVars = {
-    urls: urlDatabase[req.cookies['user_id']],
-    user: users[req.cookies['user_id']],
-    'user_id': req.cookies['user_id']
+    urls: urlDatabase[req.session.user_id],
+    user: users[req.session.user_id],
+    'user_id': req.session.user_id
   };
 
   //ignore existing cookies if they don't correspond with a user in user database
-  if (users[req.cookies['user_id']] === undefined) {
+  if (users[req.session.user_id] === undefined) {
     res.send('Please log in or register.');
   } else {
     res.render("urls_index", templateVars);
@@ -63,12 +68,12 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
 
   let templateVars = {
-    user: users[req.cookies['user_id']],
-    'user_id': req.cookies['user_id']
+    user: users[req.session.user_id],
+    'user_id': req.session.user_id
   };
 
   //ignore existing cookies if they don't correspond with a user in user database
-  if (users[req.cookies['user_id']] === undefined) {
+  if (users[req.session.user_id] === undefined) {
     res.redirect('/login');
   } else {
     res.render("urls_new", templateVars);
@@ -79,7 +84,7 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
 
-  let urlsUser = urlsForUser(req.cookies['user_id'])
+  let urlsUser = urlsForUser(req.session.user_id);
 
   //checks if user is logged in
   if (urlsUser)
@@ -90,9 +95,9 @@ app.get("/urls/:id", (req, res) => {
        let templateVars = {
         shortURL: req.params.id,
         longURL: urlsUser[req.params.id],
-        user: users[req.cookies['user_id']]
+        user_id: req.session.user_id,
+        user: users[req.session.user_id]
       };
-
       return res.render("urls_show", templateVars);
     }
   }
@@ -112,32 +117,32 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get(`/urls/:id/edit`, (req, res) => {
-  res.redirect(`/urls/${req.params.id}`);
+  res.redirect('/urls');
 });
 
 
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  urlDatabase[req.cookies['user_id']][shortURL] = req.body.longURL;
+  urlDatabase[req.session.user_id][shortURL] = req.body.longURL;
   let templateVars = {
     longURL: req.body.longURL,
     shortURL: shortURL
   }
 
-  res.redirect(`/urls/${shortURL}`);
+  res.redirect('/urls');
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   let shortURL = req.params.id;
-  delete urlDatabase[req.cookies['user_id']][shortURL];
+  delete urlDatabase[req.session.user_id][shortURL];
   res.redirect('/urls');
 });
 
 
 app.post("/urls/:id/edit", (req, res) => {
-  if (req.params.id in urlsForUser(req.cookies['user_id'])) {
-    urlDatabase[req.cookies['user_id']][req.params.id] = req.body.longURL;
+  if (req.params.id in urlsForUser(req.session.user_id)) {
+    urlDatabase[req.session.user_id][req.params.id] = req.body.longURL;
     res.redirect('/urls');
   } else {
     res.status(403).send('Invalid link.');
@@ -149,7 +154,7 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.render('user_login');
 });
 
@@ -159,16 +164,15 @@ app.post("/login", (req, res) => {
     let userEmail = users[user]['email'];
     let userPassword = users[user]['password'];
     if (req.body.email === userEmail && bcrypt.compareSync(req.body.password, userPassword)) {
-      res.cookie('user_id', users[user]['id']);
-      res.redirect('/urls');
+      req.session.user_id = users[user]['id'];
+      res.redirect('/urls/new');
     }
   }
     res.status(403).send('Invalid username and password combination.');
   });
 
-//LoGOUT
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect('urls');
 });
 
@@ -197,8 +201,7 @@ app.post("/register", (req, res) => {
       };
 
       urlDatabase[newId] = {};
-
-      res.cookie('user_id', users[newId]['id']);
+      req.session.user_id = users[newId]['id'];
       res.redirect('/urls');
 
   }
